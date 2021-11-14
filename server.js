3fc1a236d0141f7594f8f8c32d5bf82877f93e1d
Sophia Barrett <sophia@yoursummit.media>
2021-11-14 16:56:06 -0700

    add heroku

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const { animals } = require('./data/animals.json');
+
+// instantiate server
+const app = express();
+const port = 3001;
+
+function filterByQuery(query, animalsArray) {
+    let filteredResults = animalsArray;
+    let personalityTraitsArray = [];
+    if (query.personalityTraits) {
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(animal => animal.personalityTraits.indexOf(trait) !== -1);
+        });
+    }
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
+
+
+
+
+// add route
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+// tell server to listen for requests
+app.listen(port, () => {
+    console.log(`API server now on port ${port}!`);
+});
